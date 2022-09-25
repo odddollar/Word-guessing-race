f494d101d3aefac781936281ff6c435482912e34
Simon Eason <sie.eason@gmail.com>
2022-09-25 17:43:05 +0800

    Removed need for template definition

--- routes.go
+++ routes.go
@@ -8,7 +8,7 @@ import (
 )
 
 func home(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "views/home.html", gin.H{})
+	ctx.HTML(http.StatusOK, "home.html", gin.H{})
 }
 
 func homePostback(ctx *gin.Context) {
@@ -20,7 +20,7 @@ func homePostback(ctx *gin.Context) {
 }
 
 func game(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "views/game.html", gin.H{
+	ctx.HTML(http.StatusOK, "game.html", gin.H{
 		"WordList": string(wordList),
 	})
 }
